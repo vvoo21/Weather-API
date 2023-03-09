6acef8d212117a012462baa7d5ad7c5e12a69499
Vanessa O <vaneolipa@gmail.com>
2023-03-09 14:36:37 -0500

    Add event to form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+import { form } from './modules/variables.js';
+import { searchWeather } from './modules/functions.js';
+
+window.addEventListener('load', () => {
+  form.addEventListener('submit', searchWeather);
+})
